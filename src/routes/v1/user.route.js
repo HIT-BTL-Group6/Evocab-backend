a539b949b5a225e129578880d93f0faa91b76500
thuha <thuharosy2003@gmail.com>
2023-07-09 18:42:17 +0700

    :construction: Create CRUD User operations

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -2,13 +2,14 @@ const express = require('express');
 const validate = require('../../middlewares/validate.middleware');
 const { userValidation } = require('../../validations');
 const { userController } = require('../../controllers');
+const roles = require('../../middlewares/role.middleware');
 
 const userRouter = express.Router();
 
 userRouter
     .route('/')
-    .get(validate(userValidation.getUsers), userController.getUsers)
-    .post(validate(userValidation.createUser), userController.createUser);
+    .get(roles('admin'), validate(userValidation.getUsers), userController.getUsers)
+    .post(roles('admin'), validate(userValidation.createUser), userController.createUser);
 
 userRouter
     .route('/:userId')
