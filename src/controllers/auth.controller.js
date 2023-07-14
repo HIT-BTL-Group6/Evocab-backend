1519da89e6d08512e3d7c87445846db7ef94eb0b
Thu Hà <97902111+haniekid@users.noreply.github.com>
2023-07-14 22:49:43 +0700

    #14 implement forgot password (#15)
    * :construction: Implement forgot password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
-const { authService, tokenService, userService } = require('../services/index');
+const { authService, tokenService, userService, emailService } = require('../services/index');
 
 const register = catchAsync(async (req, res, next) => {
     const userBpdy = req.body;
@@ -23,7 +23,27 @@ const login = catchAsync(async (req, res, next) => {
     });
 });
 
+const forgotPassword = catchAsync(async (req, res, next) => {
+    const { email } = req.body;
+    const user = await authService.forgotPassword(email);
+    await emailService.sendResetPasswordEmail(email, user.username, user.emailToken, 'Đặt lại mật khẩu');
+    return res.json({
+        emailToken: user.emailToken,
+    });
+});
+
+const resetPassword = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    await authService.resetPassword(email, password);
+    return res.status(200).json({
+        status: httpStatus.NO_CONTENT,
+        message: 'Đặt lại mật khẩu thành công!',
+    });
+});
+
 module.exports = {
     login,
     register,
+    forgotPassword,
+    resetPassword,
 };
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,7 @@
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
-const { User } = require('../models');
+const { User, Token } = require('../models');
+const tokenTypes = require('../config/tokens');
 
 const login = async (username, password) => {
     const user = await User.findOne({
@@ -12,6 +13,40 @@ const login = async (username, password) => {
     return user;
 };
 
+const forgotPassword = async (email) => {
+    const user = await User.findOne({
+        email,
+        isEmailVerified: true,
+    });
+    if (!user) {
+        throw new ApiError('Email chưa được xác thực', httpStatus.NOT_FOUND);
+    }
+
+    const min = 100000;
+    const max = 999999;
+    const emailToken = Math.floor(Math.random() * (max - min + 1)) + min;
+    user.emailExpires = Date.now() + 1 * 60 * 1000;
+    user.emailToken = emailToken;
+
+    await user.save();
+    return user;
+};
+
+const resetPassword = async (email, password) => {
+    const user = await User.findOne({
+        email,
+        isEmailVerified: true,
+    });
+    if (!user) {
+        throw new ApiError('Email không đúng', httpStatus.NOT_FOUND);
+    }
+    user.password = password;
+    await Token.deleteMany({ user: user.id, type: tokenTypes.RESET_PASSWORD });
+    return await user.save();
+};
+
 module.exports = {
     login,
+    forgotPassword,
+    resetPassword,
 };
