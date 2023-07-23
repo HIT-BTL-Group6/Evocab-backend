80fd2ccc749806aef87e6460c32a3316cff1b0f6
Moc_Meoo <107600465+Nguyen-Duy-Son@users.noreply.github.com>
2023-07-23 10:52:27 +0700

    Implement-create-API-relate-user-and-word (#20)

---
+++ src/models/userWord.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+const Schema = mongoose.Schema;
+const userWordSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'Please provide the userID!'],
+        },
+        wordId: [{ type: Schema.Types.ObjectId, ref: 'Word', required: [true, 'Please provide the wordID!'] }],
+    },
+    {
+        timestamps: true,
+    }
+);
+// add plugin that converts mongoose to json
+userWordSchema.plugin(toJSON);
+userWordSchema.plugin(paginate);
+
+const UserWord = mongoose.model('UseWord', userWordSchema);
+
+module.exports = UserWord;
