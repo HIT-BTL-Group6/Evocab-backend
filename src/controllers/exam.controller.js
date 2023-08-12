c93d91fba244c9f1952cf0fc32fb246829bf0c1c
Nguyen-Duy-Son <nguyenduyson28072003@gmail.com>
2023-08-12 14:40:45 +0700

    Update exam.controller

--- src/controllers/exam.controller.js
+++ src/controllers/exam.controller.js
@@ -5,18 +5,47 @@ const pick = require('../utils/pick');
 const Exam = require('../models/exam.model');
 const { examService } = require('../services');
 
+// const getExams = catchAsync(async (req, res) => {
+//     const options = pick(req.query, ['sortBy', 'limit', 'page']);
+
+//     const page = parseInt(options.page) || 1;
+//     const limit = parseInt(options.limit) || 10;
+//     const skip = (page - 1) * limit;
+//     const exams = await Exam.find().limit(limit).skip(skip).populate('questions');
+    
+//     res.status(httpStatus.OK).json({
+//         code: httpStatus.OK,
+//         message: 'Get exams successfully!',
+//         data: exams,
+//     });
+// });
 const getExams = catchAsync(async (req, res) => {
     const options = pick(req.query, ['sortBy', 'limit', 'page']);
+    const { topicId } = req.query;
 
     const page = parseInt(options.page) || 1;
     const limit = parseInt(options.limit) || 10;
     const skip = (page - 1) * limit;
-    const exams = await Exam.find().limit(limit).skip(skip).populate('questions');
 
+    const exams = await Exam.find().limit(limit).skip(skip).populate('questions');
+    if(!exams){
+        throw new ApiError(httpStatus.NOT_FOUND, 'Exam not found!');
+    }
+    let filteredExams = exams;
+
+    if (topicId) {
+        filteredExams = exams.filter(exam => {
+            const topics = new Set(exam.questions.map(question => question.topic.toString()));
+            return topics.size === 1 && topics.has(topicId);
+        });
+        if (!filteredExams) {
+            throw new ApiError(httpStatus.NOT_FOUND, 'Exam not found!');
+        } 
+    }
     res.status(httpStatus.OK).json({
         code: httpStatus.OK,
         message: 'Get exams successfully!',
-        data: exams,
+        data: filteredExams,
     });
 });
 
